35039901d36eef329ab8307cf22d7d3964735ab5
Susan Pommer <kspommer@gmail.com>
2018-05-30 21:28:57 -0700

    Add name of open card to open card array

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 /*
  * Create a list that holds all of your cards
- */
+*/
  // Build HTML collection of elements with class = cards
 var cardsHtmlCollection = document.getElementsByClassName('card');
 
@@ -43,11 +43,9 @@ resetButton.addEventListener("click", function() {
 	// create new deck with class=deck
 	var newDeck = document.createElement('ul');
 	newDeck.classList.add("deck");
-	console.log(newDeck); // REMOVE 
 
 	// call function to suffle cards
 	shuffledCardNames = shuffle(cardNames); 
-	console.log(shuffledCardNames); // REMOVE
 
 	// refresh display of cards on screen
 	refreshDeckHTML(shuffledCardNames, newDeck);
@@ -68,15 +66,24 @@ resetButton.addEventListener("click", function() {
 	// convert NodeList to an array to use
 	var cardNodesArray = [].slice.call(cardNodesList);
 	// create array to hold all open cards
-	var openCardsNames = [];
+	var openCardNames = [];
+
+ // Set up the event listener on each card. If a card is clicked:
+ // - display the card's symbol (put this functionality in another function that you call from this one)
+ // - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ // - if the list already has another card, check to see if the two cards match
 
 	cardNodesArray.forEach(function(card) {
 		card.addEventListener("click", function() { 
 			// if else to determine if need to check match / reset match counter 
 			if (numberOfCardClicks < 2) {
-				console.log("less");
-				// flip card (add class="open", class="show" - see CSS)
-				this.classList.add("open", "show");
+				// flip card (add class="open", class="show" (see CSS)
+				card.classList.add("open", "show");
+				// determine card name
+				var openCard = card.querySelector("i").className;
+				// add open card to open card array
+				openCardNames.push(openCard);
+				console.log(openCardNames);
 				// increment counters
 				numberOfCardClicks = numberOfCardClicks + 1; 
 				moveCounter = moveCounter + 1;
@@ -84,16 +91,20 @@ resetButton.addEventListener("click", function() {
 			else if (numberOfCardClicks == 2) {
 				// do not flip card
 				// do not change move counter
-				console.log("2");
-				// function checkMatch()
-			}		
+				console.log("2"); // REMOVE
+				// call function to check for match  
+				checkMatch();
+			}
+			else {
+				console.log("what do we do here?") // REMOVE	
+			}
 		});	
 	});	
 }); 
 
-//function checkMatch() {
-
-//}
+function checkMatch() {
+	console.log("MATCH!") // REMOVE 
+}
 	
 
 // FUNCTIONS //
@@ -120,23 +131,17 @@ function shuffle(array) {
 	// display new reshuffled cards
 function refreshDeckHTML(shuffledCardNames, newDeck) {
 	for (i = 0; i < shuffledCardNames.length; i++) {
-
 		// create new li element 
 		var newCard = document.createElement('li')
-		
 		// add "class=card" to li element
 		newCard.classList.add("card");
-
 		// create new <i> element with class fa and card name
 		// e.g. <i class "fa fa-bomb"></i>
 		var cardContent = shuffledCardNames[i];
-
 		// insert i element into li element  
 		newCard.appendChild(cardContent);
-		
 		// add to card newDeck
 		newDeck.appendChild(newCard);
-
 	}
 	// add new deck to <div class="container"> to get to display
 	var divBody = document.querySelector('.container')
@@ -155,109 +160,53 @@ function resetNumberMoves() {
 	moves.textContent = 0; 
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+/* 
+ * When the open card array has two cards, check to see if the two cards match
+ * + if the cards match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ * + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ * + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 */
-
-/*function openCard(numberOfCardClicks, moveCounter) {
-	// initialize counter variables
-	var numberOfCardClicks = 0;
-	var moveCounter = 0;
-
-	if (numberOfCardClicks < 2) {
-		console.log("less");	
-		// flip card (add class="open", class="show" - see CSS)
-		card.classList.add(".open", ".show");
-		// increment counters
-		numberOfCardClicks = numberOfCardClicks + 1; 
-		moveCounter = moveCounter + 1; 
-		// add card to open card list array
-	};	
-	else if (numberOfCardClicks === 2) {
-		console.log("2");
-		// function checkMatch()
-	};	
-	else {
-		console.log(">2");
-		var numberOfCardClicks = 0; 
-	};	
-};
-*/
-
-/*
-// when eventlistener for card click is clicked call this function 
-// display the card's symbol 
-// change from CSS for black card (back) to CSS for card with symbol (face)
-
-}
-
-function closeCard() {
-// if card symbol does not have a match in openCardsList
-// change from CSS card with symbol (face) to CSS for black card (back)
-// remove symbol from openCardsList
-
-}
-
-function openCardHold {
-// if card symbol finds match in openCardsList
-// lock card open -- on click, nothing happens
-
-}
-
-function openCardsList {
-// when eventlistener for card click is clicked call this function 
-// add the card symbol to a list of open cards
-// list must be initialized above 
-// append list with card symbol
-
-}
-
-function checkOpenCardsList {
-// when eventlistener for card clicked and openCardsList created, then call this function
-// check appended card symbol with other symbols in list
-// if match, call openCardHold
-// if match, call matchCounter function
+function checkMatch() {
+// if match, call matchedCardsLock
+// if match, increment matchCounter
 // if no match, remove appended card from list
 // if not match, call closeCard function to flip to back) 
+
+
 }
 
-
-function moveCounter {
-// when eventlistener for card click is clicked call this function 
-// initialize this counter above   moveCounter = 0; 
-// moveCounter = moveCounter + 1;
-// change class = moves.innerHTML = +1
-}
-
-function matchCounter() {
-// initialize above matchCounter = 0; 
-// call this function when match is found in checkOpenCardsList
+function matchedCardsLock() {
 // matchCounter = matchCounter + 1;
+
 }
+
+
+
+function closeCard() {
+// if card symbol does not have a match
+// change from CSS card with symbol (face) to CSS for black card (back)
+// remove card from openCards array -- CHECK NAME HERE
+}
+
+
+
 
 //https://albert-gonzalez.github.io/easytimer.js/
-function startTimer{
+function startTimer(){
 // on first click (moveCounter = 1), of eventlistener, 
 // start timer
 // display timer in CSS
 
 }
 
-function stopTimer {
+function stopTimer() {
 // when matchCounter = 8 
 // end timer
 // display final time in CSS
 }
 
-function resetTimer {
+function resetTimer() {
 // when reset button is hit
 // stops timer if running
 // resets timer = 00.00.00
@@ -278,4 +227,8 @@ function starRating() {
 // if move counter < X display three stars 
 // note:  more stars is good :-) 
 }
-*/
+
+function displayNumberMoves() {
+
+
+}
